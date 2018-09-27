47a6e07c812fa4e28c676928180f2eae38d30f81
paulhenri-l <25308170+paulhenri-l@users.noreply.github.com>
2018-09-28 00:00:06 +0200

    TypeMap using Relation's morph map

--- src/TypeMap.php
+++ src/TypeMap.php
@@ -2,7 +2,27 @@
 
 namespace PHL\LaravelSTI;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
+
 class TypeMap
 {
-    //
+    /**
+     * Return the class name that is associated with the given alias.
+     */
+    public static function getClassName($alias)
+    {
+        return Relation::getMorphedModel($alias);
+    }
+
+    /**
+     * Return the alias that is associated with the given class name.
+     */
+    public static function getAlias($searchedClassName)
+    {
+        $results = array_filter(Relation::$morphMap, function ($className) use ($searchedClassName) {
+            return $className == $searchedClassName;
+        });
+
+        return key($results);
+    }
 }
--- tests/Unit/TypeMapTest.php
+++ tests/Unit/TypeMapTest.php
@@ -2,7 +2,53 @@
 
 namespace Tests\Unit;
 
-class TypeMapTest
+use Illuminate\Database\Eloquent\Relations\Relation;
+use PHL\LaravelSTI\TypeMap;
+use Tests\Fakes\PremiumMember;
+use Tests\Fakes\RegularMember;
+use Tests\TestCase;
+
+class TypeMapTest extends TestCase
 {
-    //
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Relation::morphMap([
+            'regular_member' => RegularMember::class,
+            'premium_member' => PremiumMember::class
+        ]);
+    }
+
+    /**
+     * You can get the class name from an alias.
+     */
+    public function testGetClassNameFromAlias()
+    {
+        $this->assertEquals(
+            RegularMember::class,
+            TypeMap::getClassName('regular_member')
+        );
+
+        $this->assertEquals(
+            PremiumMember::class,
+            TypeMap::getClassName('premium_member')
+        );
+    }
+
+    /**
+     * You can get the alias from a class name.
+     */
+    public function testGetAliasFromClassName()
+    {
+        $this->assertEquals(
+            'regular_member',
+            TypeMap::getAlias(RegularMember::class)
+        );
+
+        $this->assertEquals(
+            'premium_member',
+            TypeMap::getAlias(PremiumMember::class)
+        );
+    }
 }
